11ce7ac028cc0ac522f979cfd06a0547b7f20b61
Vivek <vivek77ravi@gmail.com>
2020-07-21 14:34:30 +0530

    updatedProperLinks

--- js/index.js
+++ js/index.js
@@ -10,3 +10,15 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+const openBusBooker=()=>{
+    window.open("https://codesandbox.io/embed/busbooker-backend-q2qpt?fontsize=14&hidenavigation=1&theme=dark");
+   
+    window.open("https://codesandbox.io/embed/busbooker-github-25o83?fontsize=14&hidenavigation=1&theme=dark");
+}
+
+const openLibrary=()=>{
+    window.open("https://codesandbox.io/embed/react-libraryapp-github-mdwxj?fontsize=14&hidenavigation=1&theme=dark");
+    window.open('https://codesandbox.io/embed/libraryapp-expressjs-github-jh783?fontsize=14&hidenavigation=1&theme=dark');
+
+}
